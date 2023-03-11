b6b4aaa3e16b5412e7be325964589e5c2fdb1bc8
Szymon Zyguła <szymon@zygula.eu>
2023-03-11 13:25:12 +0100

    Add projection matrix, make more use of nalgebra

--- src/math/affine/transforms.rs
+++ src/math/affine/transforms.rs
@@ -1,9 +1,7 @@
-use nalgebra::{SMatrix, Vector3};
+use nalgebra::{Matrix4, Vector3};
 
-pub type AffineTransform = SMatrix<f64, 4, 4>;
-
-pub fn rotate_x(angle: f64) -> AffineTransform {
-    let mut rot_x = AffineTransform::zeros();
+pub fn rotate_x(angle: f64) -> Matrix4<f64> {
+    let mut rot_x = Matrix4::zeros();
 
     rot_x[(0, 0)] = 1.0;
     rot_x[(3, 3)] = 1.0;
@@ -16,8 +14,8 @@ pub fn rotate_x(angle: f64) -> AffineTransform {
     rot_x
 }
 
-pub fn rotate_y(angle: f64) -> AffineTransform {
-    let mut rot_y = AffineTransform::zeros();
+pub fn rotate_y(angle: f64) -> Matrix4<f64> {
+    let mut rot_y = Matrix4::zeros();
 
     rot_y[(1, 1)] = 1.0;
     rot_y[(3, 3)] = 1.0;
@@ -30,8 +28,8 @@ pub fn rotate_y(angle: f64) -> AffineTransform {
     rot_y
 }
 
-pub fn rotate_z(angle: f64) -> AffineTransform {
-    let mut rot_z = AffineTransform::zeros();
+pub fn rotate_z(angle: f64) -> Matrix4<f64> {
+    let mut rot_z = Matrix4::zeros();
 
     rot_z[(2, 2)] = 1.0;
     rot_z[(3, 3)] = 1.0;
@@ -44,8 +42,8 @@ pub fn rotate_z(angle: f64) -> AffineTransform {
     rot_z
 }
 
-pub fn translate(vector: Vector3<f64>) -> AffineTransform {
-    let mut translation = AffineTransform::identity();
+pub fn translate(vector: Vector3<f64>) -> Matrix4<f64> {
+    let mut translation = Matrix4::identity();
 
     translation[(0, 3)] = vector[0];
     translation[(1, 3)] = vector[1];
@@ -54,8 +52,8 @@ pub fn translate(vector: Vector3<f64>) -> AffineTransform {
     translation
 }
 
-pub fn scale(sx: f64, sy: f64, sz: f64) -> AffineTransform {
-    let mut scaling = AffineTransform::zeros();
+pub fn scale(sx: f64, sy: f64, sz: f64) -> Matrix4<f64> {
+    let mut scaling = Matrix4::zeros();
 
     scaling[(0, 0)] = sx;
     scaling[(1, 1)] = sy;
@@ -63,4 +61,19 @@ pub fn scale(sx: f64, sy: f64, sz: f64) -> AffineTransform {
     scaling[(3, 3)] = 1.0;
 
     scaling
+}
+
+pub fn projection(fov: f32, aspect_ration: f32, near_plane: f32, far_plane: f32) -> Matrix4<f32> {
+    let mut projection_matrix = Matrix4::zeros();
+
+    let ctg_fov_over_2 = 1.0 / (fov / 2.0).tan();
+    let view_distance = far_plane - near_plane;
+
+    projection_matrix[(0, 0)] = ctg_fov_over_2 / aspect_ration;
+    projection_matrix[(1, 1)] = ctg_fov_over_2;
+    projection_matrix[(2, 2)] = (far_plane + near_plane) / view_distance;
+    projection_matrix[(2, 3)] = -2.0 * far_plane * near_plane / view_distance;
+    projection_matrix[(3, 2)] = 1.0;
+
+    projection_matrix
 }
