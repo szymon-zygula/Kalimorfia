a0c029856064bcc019ce4aa4ade7ff564cb87898
Szymon Zyguła <szymon@zygula.eu>
2023-03-11 19:33:48 +0100

    Add `Drawable` trait

--- src/math/geometry/parametric_form.rs
+++ src/math/geometry/parametric_form.rs
@@ -8,7 +8,7 @@ pub trait ParametricForm<const IN_DIM: usize, const OUT_DIM: usize> {
 }
 
 impl<T: ParametricForm<2, 3>> Gridable for T {
-    fn grid(&self, points_x: u32, points_y: u32) -> (Vec<Point3<f64>>, Vec<(u32, u32)>) {
+    fn grid(&self, points_x: u32, points_y: u32) -> (Vec<Point3<f32>>, Vec<(u32, u32)>) {
         let point_count = points_x * points_y;
         let mut points = Vec::with_capacity(point_count as usize);
         let mut indices = Vec::with_capacity(2 * point_count as usize);
@@ -21,9 +21,10 @@ impl<T: ParametricForm<2, 3>> Gridable for T {
                 let y_range = Self::PARAMETER_BOUNDS.y.1 - Self::PARAMETER_BOUNDS.y.0;
                 let y = y_idx as f64 / points_y as f64 * y_range + Self::PARAMETER_BOUNDS.y.0;
 
-                points.push(self.parametric(&Vector2::new(x, y)));
-                let point_idx = x_idx * y_idx;
+                let point = self.parametric(&Vector2::new(x, y));
+                points.push(Point3::new(point.x as f32, point.y as f32, point.z as f32));
 
+                let point_idx = x_idx * y_idx;
                 indices.push((point_idx, (point_idx + 1) % point_count));
                 indices.push((point_idx, (point_idx + points_y) % point_count));
             }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,2 +1,4 @@
 pub mod shader;
 pub mod gl_program;
+pub mod drawable;
+pub mod mesh;
