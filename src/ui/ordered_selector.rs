4dd96083604cd8b293f63d6a02d1186e96ab69b6
Szymon Zyguła <szymon@zygula.eu>
2023-03-29 06:50:15 +0200

    Add point ordering

--- src/ui/ordered_selector.rs
+++ src/ui/ordered_selector.rs
@@ -2,14 +2,42 @@ pub fn ordered_selelector(
     ui: &imgui::Ui,
     elements: Vec<(usize, String, bool)>,
 ) -> Vec<(usize, bool)> {
-    ui.columns(3, "columns", false);
-    elements
-        .iter()
-        .map(|(id, name, selected)| {
-            (
-                *id,
-                ui.selectable_config(name).selected(*selected).build() ^ *selected,
-            )
-        })
-        .collect()
+    let mut new_selects = Vec::with_capacity(elements.len());
+    let mut swaps = Vec::new();
+
+    for i in 0..elements.len() {
+        let (id, ref name, selected) = elements[i];
+        let _token = ui.push_id(format!("{}_entity", name));
+
+        if selected {
+            ui.columns(3, "columns", false);
+        }
+
+        new_selects.push((
+            id,
+            ui.selectable_config(name).selected(selected).build() ^ selected,
+        ));
+
+        if selected {
+            ui.next_column();
+            ui.set_current_column_width(30.0);
+            if i != 0 && ui.button("^") {
+                swaps.push((i, i - 1));
+            }
+
+            ui.next_column();
+            ui.set_current_column_width(30.0);
+            if i != elements.len() - 1 && elements[i + 1].2 && ui.button("v") {
+                swaps.push((i, i + 1));
+            }
+
+            ui.next_column();
+        }
+    }
+
+    for (idx1, idx2) in swaps {
+        new_selects.swap(idx1, idx2);
+    }
+
+    new_selects
 }
