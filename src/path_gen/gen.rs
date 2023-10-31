f6b9590d96436dded11db0cd36c6261cda71fe22
Szymon Zyguła <szymon@zygula.eu>
2023-10-31 19:51:50 +0100

    Add groundwork for grass mowing

--- src/path_gen/gen.rs
+++ src/path_gen/gen.rs
@@ -1,18 +1,27 @@
 use super::model::{Model, BLOCK_SIZE, MODEL_SCALE, PLANE_CENTER};
-use crate::cnc::{
-    block::Block,
-    mill::{Cutter, CutterShape},
-    program as cncp,
+use crate::{
+    cnc::{
+        block::Block,
+        mill::{Cutter, CutterShape},
+        program as cncp,
+    },
+    math::geometry::intersection::{Intersection, IntersectionPoint},
 };
 use itertools::Itertools;
 use nalgebra::{vector, Vector3};
+use ordered_float::NotNan;
 use rayon::prelude::*;
+use std::collections::BTreeMap;
 
 const SAFE_HEIGHT: f32 = 66.0;
 const CUTTER_DIAMETER_ROUGH: f32 = 16.0;
 const CUTTER_RADIUS_ROUGH: f32 = CUTTER_DIAMETER_ROUGH * 0.5;
 const CUTTER_RADIUS_ROUGH_SQRT_2: f32 = CUTTER_RADIUS_ROUGH * std::f32::consts::SQRT_2 * 0.5;
 const BASE_HEIGHT: f32 = 16.0;
+
+const CUTTER_DIAMETER_FLAT: f32 = 10.0;
+const CUTTER_RADIUS_FLAT: f32 = 0.5 * CUTTER_DIAMETER_FLAT;
+const CUTTER_HEIGHT_FLAT: f32 = 4.0 * CUTTER_DIAMETER_FLAT;
 
 pub fn rough(model: &Model) -> cncp::Program {
     const UPPER_PLANE_HEIGHT: f32 = 35.0;
@@ -143,58 +152,87 @@ fn rough_line(
 }
 
 pub fn flat(model: &Model) -> Option<cncp::Program> {
-    const CUTTER_DIAMETER: f32 = 10.0;
-    const CUTTER_RADIUS: f32 = 0.5 * CUTTER_DIAMETER;
-    const CUTTER_HEIGHT: f32 = 4.0 * CUTTER_DIAMETER;
     const FLAT_EPS: f32 = 0.1;
-
-    let silhouette = model.silhouette()?;
 
     let mut locs = initial_locations();
     locs.extend_from_slice(&[
         vector![
-            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER * 2.0,
-            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER * 2.0,
+            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER_FLAT * 2.0,
+            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER_FLAT * 2.0,
             SAFE_HEIGHT
         ],
         vector![
-            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER * 2.0,
-            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER * 2.0,
+            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER_FLAT * 2.0,
+            BLOCK_SIZE * 0.5 + CUTTER_DIAMETER_FLAT * 2.0,
             BASE_HEIGHT
         ],
     ]);
 
-    let len = silhouette.points.len();
+    let silhouette = model.silhouette()?;
 
-    for (a, b) in silhouette
-        .points
-        .into_iter()
-        .map(|p| p.point.xz())
-        .cycle()
-        .skip(len / 2) // Model-specific things
-        .take(len)
-        .tuple_windows()
-    {
-        let center = vector![
-            ((a.x + b.x) * 0.5 - PLANE_CENTER[0]) as f32 * MODEL_SCALE,
-            ((a.y + b.y) * 0.5 - PLANE_CENTER[2]) as f32 * MODEL_SCALE,
-            BASE_HEIGHT
-        ];
-        let normal =
-            vector![(-a.y + b.y) as f32, (a.x - b.x) as f32, 0.0].normalize() * CUTTER_RADIUS;
-        locs.push(center + normal);
-    }
+    locs.extend(flat_mow(&silhouette));
+    locs.extend(flat_silhouette(&silhouette)?);
 
     add_ending_locs(&mut locs);
 
     Some(cncp::Program::from_locations(
         locs,
         Cutter {
-            height: CUTTER_HEIGHT,
-            diameter: CUTTER_DIAMETER,
+            height: CUTTER_HEIGHT_FLAT,
+            diameter: CUTTER_DIAMETER_FLAT,
             shape: CutterShape::Cylinder,
         },
     ))
+}
+
+fn flat_mow(silhouette: &Intersection) -> Vec<Vector3<f32>> {
+    let (bottom, top) = silhouette
+        .points
+        .iter()
+        .map(|p| (NotNan::new(p.point.y).unwrap(), *p))
+        .partition::<BTreeMap<NotNan<f64>, IntersectionPoint>, _>(|(_, p)| p.point.x > 0.0);
+
+    let mut locs = flat_partition_paths(bottom);
+    locs.extend(flat_partition_paths(top));
+    locs
+}
+
+fn flat_partition_paths(border: BTreeMap<NotNan<f64>, IntersectionPoint>) -> Vec<Vector3<f32>> {
+    let mut locs = Vec::new();
+
+    
+
+    locs
+}
+
+fn flat_partition_path(y: f64, locs: &mut Vec<Vector3<f32>>) {
+
+}
+
+fn flat_silhouette(silhouette: &Intersection) -> Option<Vec<Vector3<f32>>> {
+    let len = silhouette.points.len();
+
+    Some(
+        silhouette
+            .points
+            .iter()
+            .map(|p| p.point.xz())
+            .cycle()
+            .skip(len / 2) // Model-specific things
+            .take(len)
+            .tuple_windows()
+            .map(|(a, b)| {
+                let center = vector![
+                    ((a.x + b.x) * 0.5 - PLANE_CENTER[0]) as f32 * MODEL_SCALE,
+                    ((a.y + b.y) * 0.5 - PLANE_CENTER[2]) as f32 * MODEL_SCALE,
+                    BASE_HEIGHT
+                ];
+                let normal = vector![(-a.y + b.y) as f32, (a.x - b.x) as f32, 0.0].normalize()
+                    * CUTTER_RADIUS_FLAT;
+                center + normal
+            })
+            .collect(),
+    )
 }
 
 pub fn detail(model: &Model) -> cncp::Program {
